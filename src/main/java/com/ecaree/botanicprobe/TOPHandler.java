00a07ecd108ba7ed3e5b3f2890677a07a24d94f4
Ecaree <2551728057@qq.com>
2024-03-27 21:32:15 +0800

    ✨ Add if statement

--- src/main/java/com/ecaree/botanicprobe/TOPHandler.java
+++ src/main/java/com/ecaree/botanicprobe/TOPHandler.java
@@ -3,6 +3,7 @@ package com.ecaree.botanicprobe;
 import com.ecaree.botanicprobe.botania.*;
 import com.ecaree.botanicprobe.botanicalmachinery.*;
 import mcjty.theoneprobe.api.ITheOneProbe;
+import net.minecraftforge.fml.ModList;
 
 import java.util.function.Function;
 
@@ -17,11 +18,13 @@ public class TOPHandler implements Function<ITheOneProbe, Void>  {
         iTheOneProbe.registerProvider(new TerraPlate());
         iTheOneProbe.registerProvider(new CellBlock());
 
-        iTheOneProbe.registerProvider(new ManaBattery());
-        iTheOneProbe.registerProvider(new MechanicalApothecary());
-        iTheOneProbe.registerProvider(new MechanicalDaisy());
-        iTheOneProbe.registerProvider(new MechanicalManaPool());
-        iTheOneProbe.registerProvider(new WorkingTile());
+        if (ModList.get().isLoaded("botanicalmachinery")) {
+            iTheOneProbe.registerProvider(new ManaBattery());
+            iTheOneProbe.registerProvider(new MechanicalApothecary());
+            iTheOneProbe.registerProvider(new MechanicalDaisy());
+            iTheOneProbe.registerProvider(new MechanicalManaPool());
+            iTheOneProbe.registerProvider(new WorkingTile());
+        }
 
         return null;
     }
