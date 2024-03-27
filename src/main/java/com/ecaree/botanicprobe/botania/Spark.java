fb05215327b6871376afc4fcddbd1e128667698e
Ecaree <2551728057@qq.com>
2024-03-27 21:32:38 +0800

    ✨ Add spark upgrade icon

--- src/main/java/com/ecaree/botanicprobe/botania/Spark.java
+++ src/main/java/com/ecaree/botanicprobe/botania/Spark.java
@@ -1,15 +1,14 @@
 package com.ecaree.botanicprobe.botania;
 
 import com.ecaree.botanicprobe.BotanicProbe;
-import mcjty.theoneprobe.api.IProbeHitEntityData;
-import mcjty.theoneprobe.api.IProbeInfo;
-import mcjty.theoneprobe.api.IProbeInfoEntityProvider;
-import mcjty.theoneprobe.api.ProbeMode;
+import mcjty.theoneprobe.api.*;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import vazkii.botania.common.entity.EntityManaSpark;
+import vazkii.botania.common.item.ModItems;
 
 public class Spark implements IProbeInfoEntityProvider {
     @Override
@@ -21,25 +20,33 @@ public class Spark implements IProbeInfoEntityProvider {
     public void addProbeEntityInfo(ProbeMode probeMode, IProbeInfo iProbeInfo, Player player, Level level, Entity entity, IProbeHitEntityData data) {
         if (entity instanceof EntityManaSpark sparkEntity) {
 
+            ItemStack itemStack = null;
             String upgradeName = "null";
             switch (sparkEntity.getUpgrade()) {
                 case DISPERSIVE:
+                    itemStack = new ItemStack(ModItems.sparkUpgradeDispersive);
                     upgradeName = I18n.get("item.botania.spark_upgrade_dispersive");
                     break;
                 case DOMINANT:
+                    itemStack = new ItemStack(ModItems.sparkUpgradeDominant);
                     upgradeName = I18n.get("item.botania.spark_upgrade_dominant");
                     break;
                 case RECESSIVE:
+                    itemStack = new ItemStack(ModItems.sparkUpgradeRecessive);
                     upgradeName = I18n.get("item.botania.spark_upgrade_recessive");
                     break;
                 case ISOLATED:
+                    itemStack = new ItemStack(ModItems.sparkUpgradeIsolated);
                     upgradeName = I18n.get("item.botania.spark_upgrade_isolated");
                     break;
                 case NONE:
                     break;
             }
-            if (!upgradeName.equals("null")) {
-                iProbeInfo.text(upgradeName);
+            if (itemStack != null && !upgradeName.equals("null")) {
+                iProbeInfo
+                        .horizontal(iProbeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
+                        .item(itemStack, iProbeInfo.defaultItemStyle().width(16).height(16))
+                        .text(upgradeName);
             }
         }
     }
