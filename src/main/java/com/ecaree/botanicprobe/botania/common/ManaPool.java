42fe416877356fb0c04fd5a0ada8f59c93383a1e
Ecaree <2551728057@qq.com>
2024-04-02 08:39:53 +0800

    ✨ Add catalyst

--- src/main/java/com/ecaree/botanicprobe/botania/common/ManaPool.java
+++ src/main/java/com/ecaree/botanicprobe/botania/common/ManaPool.java
@@ -2,11 +2,13 @@ package com.ecaree.botanicprobe.botania.common;
 
 import com.ecaree.botanicprobe.TOPUtil;
 import mcjty.theoneprobe.api.*;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.ForgeRegistries;
+import vazkii.botania.common.block.ModBlocks;
 import vazkii.botania.common.block.tile.mana.TilePool;
 
 public class ManaPool implements IProbeInfoProvider {
@@ -25,6 +27,12 @@ public class ManaPool implements IProbeInfoProvider {
                 iProbeInfo.text("Mana: ∞");
             } else {
                 iProbeInfo.text("Mana: " + mana + "/" + manaCap);
+            }
+
+            if (level.getBlockState(data.getPos().below()).is(ModBlocks.alchemyCatalyst)) {
+                iProbeInfo.text(I18n.get("botanicprobe.text.catalyst") + ModBlocks.alchemyCatalyst.getName().getString());
+            } else if (level.getBlockState(data.getPos().below()).is(ModBlocks.conjurationCatalyst)) {
+                iProbeInfo.text(I18n.get("botanicprobe.text.catalyst") + ModBlocks.conjurationCatalyst.getName().getString());
             }
         }
     }
